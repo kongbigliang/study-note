d18dda83a2b286e57b52f2c44e44ec4050167a70
kongbigliang <kongbigliang@163.com>
2019-12-08 20:27:10 +0800

    将客户端注册到eureka服务器

--- study-cloud1/eureka2/src/main/java/com/kongbig/cloud/Eureka2Application.java
+++ study-cloud1/eureka2/src/main/java/com/kongbig/cloud/Eureka2Application.java
@@ -8,6 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 注册中心
  * EnableEurekaServer：申明这是一个Eureka服务
  *
+ * 以通过停止其中一个Eureka server服务进行测试，结果会发现集群是高可用。
+ * 即：启动2个Eureka服务端(注册中心)，1个商品服务（item），1个订单服务（order）。
+ * 停掉其中一个Eureka服务端后，访问另外一个Eureka服务端页面可以看到item服务立马切换到此注册中心，
+ * 订单服务照样可以访问商品服务，浏览器输入http://localhost:8082/order/201810300001还能获取到商品数据（因为商品服务同时注册到了2个注册中心）。
+ *
  * @author lianggangda
  * @date 2019/12/6 15:14
  */
