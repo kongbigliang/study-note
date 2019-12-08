72cf16dba86d3c2ab48983693e3352666bc953d0
kongbigliang <kongbigliang@163.com>
2019-12-08 10:16:02 +0800

    eureka服务端

---
+++ study-cloud1/eureka2/src/main/java/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author kongbig
+ * @date 2019/12/8 9:30
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        // 注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录，所以必须是httpBasic
+        // 如果是form方式，不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
