e3faa097e45b19b8b8242c2d632ceace964b7d99
kongbigliang <kongbigliang@163.com>
2019-12-18 15:04:57 +0800

    Config Server快速入门

--- study-cloud1/microservice-config-server/src/main/java/com/kongbig/cloud/ConfigApplication.java
+++ study-cloud1/microservice-config-server/src/main/java/com/kongbig/cloud/ConfigApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * 开启配置服务
  *
+ * 测试的访问地址为：
+ * http://127.0.0.1:7788/microservice-test.properties
+ *
  * @author lianggangda
  * @date 2019/12/18 11:20
  */
