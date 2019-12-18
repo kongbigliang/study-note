80d6e05d4c8944bc3347aacf0e355b5cb58eee31
kongbigliang <kongbigliang@163.com>
2019-12-18 15:03:10 +0800

    Config Server快速入门

---
+++ study-cloud1/microservice-config-server/src/main/java/com/kongbig/cloud/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.kongbig.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 开启配置服务
+ *
+ * @author lianggangda
+ * @date 2019/12/18 11:20
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
