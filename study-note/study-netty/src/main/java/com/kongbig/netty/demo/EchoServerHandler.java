ec8bd3514a418f458213090ca7eabc98a713ded2
kongbigliang <kongbigliang@163.com>
2020-01-15 15:23:17 +0800

    netty相关

---
+++ study-note/study-netty/src/main/java/com/kongbig/netty/demo/EchoServerHandler.java
@@ -0,0 +1,60 @@
+package com.kongbig.netty.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 服务端处理的代码包含了三个方法。这三个方法都是根据事件触发的
+ *
+ * @author lianggangda
+ * @date 2020/1/13 11:38
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 1.当接收到消息时的操作，channelRead。
+     *
+     * @param ctx
+     * @param msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        // 将消息记录到控制台
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        // 将接收到的消息写给发送者，而不冲刷出站消息
+        ctx.write(in);
+    }
+
+    /**
+     * 2.消息读取完成时的方法，channelReadComplete。
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        // 将未决信息冲刷到远程节点，并且关闭该Channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 3.出现异常时的方法，exceptionCaught。
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 打印异常栈跟踪
+        cause.printStackTrace();
+        // 关闭该Channel
+        ctx.close();
+    }
+}
