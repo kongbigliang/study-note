5f3fc20f3234eaf86a0523424e0fe4372e6a0f27
kongbigliang <kongbigliang@163.com>
2020-05-02 10:59:21 +0800

    TestDemo

--- study-note/study-java8/src/main/java/com/kongbig/java8/bean/Employee.java
+++ study-note/study-java8/src/main/java/com/kongbig/java8/bean/Employee.java
@@ -1,9 +1,22 @@
 package com.kongbig.java8.bean;
 
+import lombok.*;
+
 /**
  * @Description:
  * @author: lianggangda
  * @date: 2020/5/2 10:25
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
+
+    private int id;
+    private String name;
+    private int age;
+    private double salary;
+
 }
--- study-note/study-java8/src/main/java/com/kongbig/java8/service/MyPredicate.java
+++ study-note/study-java8/src/main/java/com/kongbig/java8/service/MyPredicate.java
@@ -1,9 +1,20 @@
 package com.kongbig.java8.service;
 
+import com.kongbig.java8.bean.Employee;
+
 /**
  * @Description:
  * @author: lianggangda
  * @date: 2020/5/2 10:37
  */
-public class MyPredicate {
+public interface MyPredicate<T> {
+
+    /**
+     * 比较方法
+     *
+     * @param emp
+     * @return
+     */
+    public boolean test(Employee emp);
+
 }
