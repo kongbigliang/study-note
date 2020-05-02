9bf35e95dd8d9d575723b43818fb750f24eebb6b
kongbigliang <kongbigliang@163.com>
2020-05-02 15:37:14 +0800

    lambda基础语法

--- study-note/study-java8/README.md
+++ study-note/study-java8/README.md
@@ -1,3 +1,4 @@
+
 # Java8
 ## Java8 新特性
 * 速度更快
@@ -10,7 +11,37 @@
 
 ## Lambda表达式
 > Lambda 是一个匿名函数，可以把 Lambda 表达式理解为是一段可以传递的代码（将代码像数据一样进行传递）。
-
+基础语法:
+* 操作符 "->"
+    * 左侧：Lambda 表达式的参数列表
+    * 右侧：Lambda 表达式中所需执行的功能， 即 Lambda 体
+* 语法格式1：无参数，无返回值。
+    ```
+    () -> System.out.println("Hello Lambda!");
+    ```
+* 语法格式2：有一个参数，并且无返回值。
+    ```
+    (x) -> System.out.println(x)
+    ```
+* 语法格式3：若只有一个参数，小括号可以省略不写。
+    ```
+    x -> System.out.println(x)
+    ```
+* 语法格式4：有两个以上的参数，有返回值，并且 Lambda 体中有多条语句。
+    ```
+    Comparator<Integer> com = (x, y) -> {
+        System.out.println("函数式接口");
+        return Integer.compare(x, y);
+    };
+    ```
+* 语法格式5：若 Lambda 体中只有一条语句， return 和 大括号都可以省略不写。
+    ```
+    Comparator<Integer> com = (x, y) -> Integer.compare(x, y);
+    ```
+* 语法格式6：Lambda 表达式的参数列表的数据类型可以省略不写，因为JVM编译器通过上下文推断出数据类型，即“**类型推断**”。
+    ```
+    (Integer x, Integer y) -> Integer.compare(x, y);
+    ```
 
 ## 函数式接口
 ## 方法引用与构造器引用
---
+++ study-note/study-java8/src/main/java/com/kongbig/java8/lambda/TestLambda01.java
@@ -0,0 +1,50 @@
+package com.kongbig.java8.lambda;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+/**
+ * @Description:
+ * @author: lianggangda
+ * @date: 2020/5/2 15:22
+ */
+public class TestLambda01 {
+
+    @Test
+    public void test1() {
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World!");
+            }
+        };
+        r1.run();
+
+        System.out.println("------------------");
+
+        Runnable r2 = () -> System.out.println("Hello Lambda!");
+        r2.run();
+    }
+
+    @Test
+    public void test2() {
+        Consumer<String> consumer = x -> System.out.println(x);
+        consumer.accept("哈哈哈哈~");
+    }
+
+    @Test
+    public void test3() {
+        Comparator<Integer> comparator = (x, y) -> {
+            System.out.println("函数式接口");
+            return Integer.compare(x, y);
+        };
+    }
+
+    @Test
+    public void test4() {
+        Comparator<Integer> comparator = (x, y) -> Integer.compare(x, y);
+    }
+
+}
