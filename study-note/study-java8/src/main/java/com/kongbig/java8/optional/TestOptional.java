25012e652fa301ddc4e4930ab3902b41e7345a6e
kongbigliang <kongbigliang@163.com>
2020-05-04 15:57:19 +0800

    Optional类

---
+++ study-note/study-java8/src/main/java/com/kongbig/java8/optional/TestOptional.java
@@ -0,0 +1,71 @@
+package com.kongbig.java8.optional;
+
+import com.kongbig.java8.bean.Employee;
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * @Description:
+ * @author: lianggangda
+ * @date: 2020/5/4 15:35
+ */
+public class TestOptional {
+
+    /*
+     * 一、Optional 容器类：用于尽量避免空指针异常
+     * 	Optional.of(T t) : 创建一个 Optional 实例
+     * 	Optional.empty() : 创建一个空的 Optional 实例
+     * 	Optional.ofNullable(T t):若 t 不为 null,创建 Optional 实例,否则创建空实例
+     * 	isPresent() : 判断是否包含值
+     * 	orElse(T t) :  如果调用对象包含值，返回该值，否则返回t
+     * 	orElseGet(Supplier s) :如果调用对象包含值，返回该值，否则返回 s 获取的值
+     * 	map(Function f): 如果有值对其处理，并返回处理后的Optional，否则返回 Optional.empty()
+     * 	flatMap(Function mapper):与 map 类似，要求返回值必须是Optional
+     */
+
+    @Test
+    public void test1() {
+        // 能快速锁定空指针位置
+        Optional<Employee> op = Optional.of(new Employee());
+        Employee emp = op.get();
+        System.out.println(emp);
+    }
+
+    @Test
+    public void test2() {
+        Optional<Employee> op = Optional.ofNullable(null);
+        System.out.println(op.get());
+
+        Optional<Employee> op2 = Optional.empty();
+        System.out.println(op2.get());
+    }
+
+
+    @Test
+    public void test3() {
+        Optional<Employee> op = Optional.ofNullable(new Employee());
+
+        if (op.isPresent()) {
+            System.out.println(op.get());
+        }
+
+        Employee emp = op.orElse(new Employee("张三"));
+        System.out.println(emp);
+
+        Employee emp2 = op.orElseGet(() -> new Employee());
+        System.out.println(emp2);
+    }
+
+    @Test
+    public void test4() {
+        Optional<Employee> op = Optional.of(new Employee(101, "张三", 18, 9999.99));
+
+        Optional<String> op2 = op.map(Employee::getName);
+        System.out.println(op2.get());
+
+        Optional<String> op3 = op.flatMap((e) -> Optional.of(e.getName()));
+        System.out.println(op3.get());
+    }
+
+}
