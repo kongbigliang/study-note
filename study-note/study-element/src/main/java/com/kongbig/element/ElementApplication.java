bde329b377ec89e5cb17626f58410b3745814cac
kongbig <kongbigliang@163.com>
2020-07-15 10:32:50 +0800

    study element

---
+++ study-note/study-element/src/main/java/com/kongbig/element/ElementApplication.java
@@ -0,0 +1,18 @@
+package com.kongbig.element;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: lianggangda
+ * @date: 2020/7/7 10:01
+ */
+@SpringBootApplication
+public class ElementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElementApplication.class, args);
+    }
+
+}
